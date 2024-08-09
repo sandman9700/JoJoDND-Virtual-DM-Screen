12e545c77953f663218f08e1c7896e86c7ff037a
sandman9700 <150147390+sandman9700@users.noreply.github.com>
2024-08-08 20:32:05 -0400

    Table fix

--- table.js
+++ table.js
@@ -2,28 +2,26 @@ $(document).ready(function() {
     $.getJSON('actionEconomy.json', function(data) {
         let tableContent = "<thead><tr>";
 
-        // Create the table headers
         for (const category in data) {
             tableContent += "<th>" + category.replace("_", " ") + "</th>";
         }
 
         tableContent += "</tr></thead><tbody>";
 
-        // Find the maximum number of rows needed
+
         const maxRows = Math.max(...Object.values(data).map(arr => arr.length));
 
-        // Create the rows
         for (let i = 0; i < maxRows; i++) {
             tableContent += "<tr>";
             for (const category in data) {
-                tableContent += "<td>" + (data[category][i] || "") + "</td>";
+                tableContent += "<td>" + (data[category][i] || "<textarea type=text placeholder='More' class='notepad bg-warning'></textarea>") + "</td>";
             }
             tableContent += "</tr>";
         }
 
         tableContent += "</tbody>";
 
-        // Update the table's HTML
+
         $("#box").html(tableContent);
     });
 });
